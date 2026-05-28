e027c3aca1f22274a23965bb94fdffca536d2b0f
sonpt145 <soncocxomao@gmail.com>
2026-05-29 00:09:25 +0700

    section 2

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,18 @@ import posterTrauTem from "./assets/poster-trau-tem.png";
 import posterHySu from "./assets/poster-hy-su.png";
 import posterChiDo from "./assets/poster-chi-do.png";
 
+import section2Bg from "./assets/section2-bg.png";
+
+import itemHyPhuc from "./assets/item-hy-phuc.png";
+import itemTrauCau from "./assets/item-trau-cau.png";
+import itemNghiLe from "./assets/item-nghi-le.png";
+import itemTuongTac from "./assets/item-tuong-tac.png";
+
+import gallery1 from "./assets/gallery-1.png";
+import gallery2 from "./assets/gallery-2.png";
+import gallery3 from "./assets/gallery-3.png";
+import gallery4 from "./assets/gallery-4.png";
+
 function App() {
   const [zoomImg, setZoomImg] = useState(null);
 
@@ -45,6 +57,68 @@ function App() {
             <img src={zoomImg} alt="Zoom poster" />
           </div>
         )}
+      </section>
+
+      <section
+        id="explore"
+        className="exhibition-section"
+        style={{ backgroundImage: `url(${section2Bg})` }}
+      >
+        <img
+          src={itemHyPhuc}
+          className="exhibit-item item-hy-phuc"
+          onClick={() => setZoomImg(itemHyPhuc)}
+          alt=""
+        />
+
+        <img
+          src={itemTrauCau}
+          className="exhibit-item item-trau-cau"
+          onClick={() => setZoomImg(itemTrauCau)}
+          alt=""
+        />
+
+        <img
+          src={itemNghiLe}
+          className="exhibit-item item-nghi-le"
+          onClick={() => setZoomImg(itemNghiLe)}
+          alt=""
+        />
+
+        <img
+          src={itemTuongTac}
+          className="exhibit-item item-tuong-tac"
+          onClick={() => setZoomImg(itemTuongTac)}
+          alt=""
+        />
+
+        <img
+          src={gallery1}
+          className="gallery-img gallery-1"
+          onClick={() => setZoomImg(gallery1)}
+          alt=""
+        />
+
+        <img
+          src={gallery2}
+          className="gallery-img gallery-2"
+          onClick={() => setZoomImg(gallery2)}
+          alt=""
+        />
+
+        <img
+          src={gallery3}
+          className="gallery-img gallery-3"
+          onClick={() => setZoomImg(gallery3)}
+          alt=""
+        />
+
+        <img
+          src={gallery4}
+          className="gallery-img gallery-4"
+          onClick={() => setZoomImg(gallery4)}
+          alt=""
+        />
       </section>
     </>
   );
